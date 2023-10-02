e58c4cf740a422558e845ffe45237966a52fbf9f
0B0767649 <0B0767649@LAPTOP-U30DFKAC>
2023-10-02 13:36:30 -0300

    Merge branch 'main' of https://github.com/JedConOrg/IBMCodeTest.git

--- src/User.java
+++ src/User.java
@@ -1,7 +1,25 @@
 
 public class User {
     private String username;
-    private String password;
+    public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getIpAddress() {
+		return ipAddress;
+	}
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+	private String password;
     private String ipAddress;
 
     // Getters and setters
