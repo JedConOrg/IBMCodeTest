240630584844724755a9c23255284066b8844d5b
0B0767649 <0B0767649@9.67.184.26>
2023-10-16 15:08:51 -0300

    Regex working

--- src/main/java/com/jed/reguser/RegUser/Controller.java
+++ src/main/java/com/jed/reguser/RegUser/Controller.java
@@ -1,10 +1,9 @@
 package com.jed.reguser.RegUser;
 
-import org.springframework.boot.jackson.JsonObjectDeserializer;
+import java.util.regex.*;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import com.fasterxml.jackson.databind.util.JSONPObject;
 
 @RestController
 public class Controller {
@@ -15,14 +14,43 @@ public class Controller {
 	public String RegOfTheUser(
 									@RequestBody User user
 			                  ){
-		String reply;
-		reply =         "<p>Your Name is: " + user.getUserName() + "</p>";
-		reply = reply + "<p>Your password is: " + user.getPassword() + "</p>";
-		reply = reply + "<p>Your IP is: " + user.getIpAddress() + "</p>";
-	    return reply;
+
+//		String reply;
+//		reply =         "<p>Your Name is: " + user.getUserName() + "</p>";
+//		reply = reply + "<p>Your password is: " + user.getPassword() + "</p>";
+//		reply = reply + "<p>Your IP is: " + user.getIpAddress() + "</p>";
+
+		
+		if (PasswordIsGood(user.getPassword())) {
+			return "Yes!";
+		} else {
+			return "NO!";			
+		}
 	}
     
-	
+	private boolean PasswordIsGood (String pW) {
+
+		// Regex to check valid password.
+        String regex = "^(?=.*[0-9])"
+                       + "(?=.*[a-z])(?=.*[A-Z])"
+                       + "(?=.*[_#$%.])"
+                       + "(?=\\S+$).{8,}$";
+ 
+        Pattern p = Pattern.compile(regex);
+ 
+        // If the password is empty
+        // return false
+        if (pW == null) {
+            return false;
+        }
+ 
+        // Check string for matches
+        Matcher m = p.matcher(pW);
+ 
+        // return if it found matches or not.
+        return m.matches();
+
+	}
 	
 }
 
