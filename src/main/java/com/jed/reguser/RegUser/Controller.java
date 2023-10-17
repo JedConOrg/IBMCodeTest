71bde3cca88fa3bb693557f084b2f329fd162c04
0B0767649 <0B0767649@LAPTOP-U30DFKAC>
2023-10-17 14:31:55 -0300

    Working copy

--- src/main/java/com/jed/reguser/RegUser/Controller.java
+++ src/main/java/com/jed/reguser/RegUser/Controller.java
@@ -1,15 +1,26 @@
 package com.jed.reguser.RegUser;
 
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.UUID;
 import java.util.regex.*;
 
+import org.springframework.boot.jackson.JsonObjectSerializer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 @RestController
 public class Controller {
 	
-	@PostMapping ("/api/reguser"
-			)
+	@PostMapping ("/api/reguser")
 	@ResponseBody
 	public String RegOfTheUser(
 									@RequestBody User user
@@ -20,14 +31,74 @@ public class Controller {
 //		reply = reply + "<p>Your password is: " + user.getPassword() + "</p>";
 //		reply = reply + "<p>Your IP is: " + user.getIpAddress() + "</p>";
 
-		
-		if (PasswordIsGood(user.getPassword())) {
-			return "Yes!";
-		} else {
-			return "NO!";			
+		if (user.getUserName().equals(null)) {
+			return "<p>please suply a username.</p>";
 		}
+		if (!PasswordIsGood(user.getPassword())) {
+			return "<p>Invalid Password</p>";
+		}
+		
+		GeoIP geoIP;
+		try {
+			geoIP = GetGeoLoc (user.getIpAddress());
+		} catch (Exception e) {
+			return "<p>There is something wron with your IPaddress </p> <p>" + e.getMessage() + "</p>";
+		}
+
+		if (!geoIP.getCountry().equals("Canada")) {
+			return "<p>I'm sorry this application is only avaliable to Canadain Users.</p> ";
+		}
+		UUID uuid = UUID.randomUUID();
+		
+		return "<p>Good Day " + user.getUserName() +" Welcome to the application.</p><p>I see you are located in " + geoIP.getCity() +
+				", " + geoIP.getCountry() + "</p><p> Please make note of your UUID for later refrence: " + uuid + "</p><p>";
 	}
     
+	
+	private GeoIP GetGeoLoc (String ip) throws Exception {
+		
+		GeoIP myLoc = new GeoIP();
+		
+		String geoLocURL = "http://ip-api.com/json/"+ip;
+		
+		URL url = new URL (geoLocURL);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.connect();
+
+		//Getting the response code
+		int responsecode = conn.getResponseCode();
+		if (responsecode != 200) {
+		    throw new RuntimeException("HttpResponseCode: " + responsecode);
+		}
+
+		RestTemplate restTemplate = new RestTemplate();
+		try {
+			String response = restTemplate.getForObject(geoLocURL, String.class);
+			JsonElement jsonElement = JsonParser.parseString(response);
+			myLoc.setStatus(jsonElement.getAsJsonObject().get("status").getAsString());
+			myLoc.setCountry(jsonElement.getAsJsonObject().get("country").getAsString());
+			myLoc.setCity(jsonElement.getAsJsonObject().get("city").getAsString());
+			myLoc.setQuery(jsonElement.getAsJsonObject().get("query").getAsString());
+			myLoc.setCountryCode(jsonElement.getAsJsonObject().get("countryCode").getAsString());
+			myLoc.setRegion(jsonElement.getAsJsonObject().get("region").getAsString());
+			myLoc.setRegionName(jsonElement.getAsJsonObject().get("regionName").getAsString());
+			myLoc.setZip(jsonElement.getAsJsonObject().get("zip").getAsString());
+			myLoc.setLat(jsonElement.getAsJsonObject().get("lat").getAsString());
+			myLoc.setLon(jsonElement.getAsJsonObject().get("lon").getAsString());
+			myLoc.setTimezone(jsonElement.getAsJsonObject().get("timezone").getAsString());
+			myLoc.setIsp(jsonElement.getAsJsonObject().get("isp").getAsString());
+			myLoc.setOrg(jsonElement.getAsJsonObject().get("org").getAsString());
+			myLoc.setAs(jsonElement.getAsJsonObject().get("as").getAsString());
+		} catch (Exception e) {
+			throw new RuntimeException("JSON Parser: " + responsecode);
+		}
+		
+
+		return myLoc;
+	}
+	
+	
 	private boolean PasswordIsGood (String pW) {
 
 		// Regex to check valid password.
@@ -51,6 +122,6 @@ public class Controller {
         return m.matches();
 
 	}
-	
+
 }
 
